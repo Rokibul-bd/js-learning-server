97b812d5e3f7d5d627d4713abd1971f080f4d8e9
Rokibul-bd <mdrokbiulislamcmt176@gmail.com>
2022-10-26 18:14:07 +0600

    install cors and use cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.port || 5000
-
 const data = require('./data/courses.json');
+
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send("my Server is Running")
