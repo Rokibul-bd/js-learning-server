389484083293b89ebda84de0c72f69098ad9b349
Rokibul-bd <mdrokbiulislamcmt176@gmail.com>
2022-10-26 18:02:10 +0600

    create courses json file and add javascript course details and send data server

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
-
 const port = process.env.port || 5000
+
+const data = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send("my Server is Running")
+})
+
+app.get('/courses', (req, res) => {
+    res.send(data)
 })
 
 app.listen(port, () => {
